c6eb0c5fe087d2ab019f04b8e7760352642f5b38
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-02-17 16:04:01 +0200

    refactor api manager

--- app/src/main/java/com/example/newsapp/api/ApiManager.kt
+++ app/src/main/java/com/example/newsapp/api/ApiManager.kt
@@ -1,24 +1,40 @@
 package com.example.newsapp.api
 
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.logging.Level
 
 class ApiManager {
 
     companion object {
         private var retrofit: Retrofit? = null
+
         @Synchronized
         private fun getInstance(): Retrofit {
             if (retrofit == null) {
+                val loggingInterceptor = HttpLoggingInterceptor {
+                    Log.e("api", it)
+                }.apply {
+                    setLevel(HttpLoggingInterceptor.Level.BODY)
+                }
+                val okHttpClient = OkHttpClient
+                    .Builder()
+                    .addInterceptor(loggingInterceptor)
+                    .build()
                 retrofit = Retrofit.Builder()
                     .baseUrl("https://newsapi.org/")
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
                     .build()
             }
             return retrofit!!
         }
-        fun getApi() :WebServices{
-           return getInstance().create(WebServices::class.java)
+
+        fun getApi(): WebServices {
+            return getInstance().create(WebServices::class.java)
         }
 
     }
