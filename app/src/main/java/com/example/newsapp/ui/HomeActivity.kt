827d6274f6f2cbba1a1dbe0fb63837056ba38dd5
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-02-25 15:13:15 +0200

    create all fragments

--- app/src/main/java/com/example/newsapp/ui/HomeActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/HomeActivity.kt
@@ -1,29 +1,55 @@
 package com.example.newsapp.ui
+
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.newsapp.R
+import com.example.newsapp.ui.fragments.CategoriesFragment
+import com.example.newsapp.ui.fragments.SettingsFragment
 
 class HomeActivity : AppCompatActivity() {
-    lateinit var drawerLayout: DrawerLayout
-    lateinit var icMenu : ImageView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var icMenu: ImageView
+    private lateinit var categoriesTV: TextView
+    private lateinit var settingsTV: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-initViews()
+        initViews()
+        initListeners()
     }
 
-   fun initViews(){
-       drawerLayout = findViewById(R.id.drawer_layout)
-       icMenu = findViewById(R.id.ic_menu)
-   }
+    private fun initViews() {
+        drawerLayout = findViewById(R.id.drawer_layout)
+        icMenu = findViewById(R.id.ic_menu)
+        categoriesTV = findViewById(R.id.categories_tv)
+        settingsTV = findViewById(R.id.settings_tv)
+    }
 
 
-    fun initListeners(){
+   private fun initListeners() {
         icMenu.setOnClickListener {
             drawerLayout.open()
         }
+
+        categoriesTV.setOnClickListener {
+            showFragments(CategoriesFragment())
+            drawerLayout.close()
+        }
+
+        settingsTV.setOnClickListener {
+            showFragments(SettingsFragment())
+            drawerLayout.close()
+        }
+    }
+
+    fun showFragments(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fragment_container, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/newsapp/ui/fragments/CategoriesFragment.kt
@@ -0,0 +1,22 @@
+package com.example.newsapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsapp.R
+
+class CategoriesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_categories, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
